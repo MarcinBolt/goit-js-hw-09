5479c0c46917f7ecb2b54028eeb8cc2084dd4202
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-03-05 17:04:03 +0100

    solved task-01

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,26 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+stopBtn.disabled = true;
+
+
+startBtn.addEventListener("click", () => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+}, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    body.style.backgroundColor = ``;
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
